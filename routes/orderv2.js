6e6fd9ccfe5e6d8197309b4f9cc00c2ce660d31f
suresh Kumar <sureshkumar.medasani@gmail.com>
2019-06-06 13:55:53 +0530

    order v2 server code direct api call

--- routes/orderv2.js
+++ routes/orderv2.js
@@ -12,38 +12,38 @@ router.post('/create', function(req, res) {
         "intent": "CAPTURE",
         
         "payer": {
-                    "email_address": "usbuyer1@test.com"
+                    "email_address": "inbuyer1@test.com"
         },
         "purchase_units": [
             {
                 "reference_id": "ref_12311124",
                 "description": "Camera Shop",
                 "amount": {
-                    "currency_code": "USD",
+                    "currency_code": "INR",
                     "value": "25.00",
                     "breakdown": {
                         "item_total": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "25.00"
                         },
                         "shipping": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "0"
                         },
                         "handling": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "0"
                         },
                         "tax_total": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "0"
                         },
                         "gift_wrap": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "0"
                         },
                         "shipping_discount": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "0"
                         }
                     }
@@ -54,11 +54,11 @@ router.post('/create', function(req, res) {
                         "name": "Levis 501 Selvedge STF",
                         "sku": "5158936",
                         "unit_amount": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "25.00"
                         },
                         "tax": {
-                            "currency_code": "USD",
+                            "currency_code": "INR",
                             "value": "0.00"
                         },
                         "quantity": "1",
@@ -69,10 +69,10 @@ router.post('/create', function(req, res) {
                     "address": {
                         "address_line_1": "500 Hillside Street",
                         "address_line_2": "#1000",
-                        "admin_area_1": "CA",
-                        "admin_area_2": "San Jose",
-                        "postal_code": "95131",
-                        "country_code": "US"
+                        "admin_area_1": "TN",
+                        "admin_area_2": "chennai",
+                        "postal_code": "500050",
+                        "country_code": "IN"
                     }
                 },
                 "shipping_method": "United Postal Service",
@@ -86,7 +86,7 @@ router.post('/create', function(req, res) {
         "application_context": {
             "brand_name":"flipkart",
             "return_url": "https://pcp-orderv2.herokuapp.com/orderv2/success",
-            "cancel_url": "http://localhost"
+            "cancel_url": "https://pcp-orderv2.herokuapp.com/orderv2/success"
         }
     };
     var options = {
@@ -160,7 +160,7 @@ router.get('/success', function(req, res) {
             if(response.statusCode == 200 || response.statusCode == 201) {
                 res.writeHead(302,{'Location':("app://com.herokuapp.apmanative.androidecorderv2cct://success?token="+req.query.token)});       
             } else {
-                res.writeHead(302,{'Location':("app://com.herokuapp.apmanative.androidecorderv2cct://error?token="+req.query.token)});
+                res.writeHead(302,{'Location':("app://com.herokuapp.apmanative.androidecorderv2cct://success?token="+null)});
             }      
             res.end();
         });
