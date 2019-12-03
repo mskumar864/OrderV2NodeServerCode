77945d2f9b6a5307777a0924d91473bb0a989b97
suresh Kumar <sureshkumar.medasani@gmail.com>
2019-12-03 11:47:58 +0530

    partner chagnes

--- routes/config.js
+++ routes/config.js
@@ -1,7 +1,12 @@
 var request = require('request');
 var sanboxUrl = 'https://api.sandbox.paypal.com';
-var clientId = "AcuuDiWgApKeQx7oY6wuGh2kbAIzy8B1NrruTzVl_vn3Dqv7a-EYGKlHRMb70fjc3eX3EP5rlM3VUp8g"; //india-business
-var secret = "ELxGTdUhk3gGuO7nFv1sDF5waqAsUcKDoA0djtfIYgGVIXEjealGQvq93-vdWsc8rvHnNVOfeNdvngWE";
+//var clientId = "AcuuDiWgApKeQx7oY6wuGh2kbAIzy8B1NrruTzVl_vn3Dqv7a-EYGKlHRMb70fjc3eX3EP5rlM3VUp8g"; //india-business
+//var secret = "ELxGTdUhk3gGuO7nFv1sDF5waqAsUcKDoA0djtfIYgGVIXEjealGQvq93-vdWsc8rvHnNVOfeNdvngWE";
+
+var clientId = "AU4kkD6HuzjSwSkixUsqtYGnKY8bO_jIZHD4aXzBToRLxc6hjnFSbem0sNkSdY5QLBvREwDvKarxd37x"; //india-business
+var secret = "ENKMkLnQgF2IyxOM_RBrUPmXBF8TpKZHquPRnyWBgSDKYcMUC2pxXQw1OcnAB-ZCxx-mF1iVDLqLucCK";
+
+
 var basicAuth = new Buffer(clientId+":"+secret).toString('base64') ;
 
 var initialize = function(){  
